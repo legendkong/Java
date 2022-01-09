cdebf9cee071f5a0d2e3d4469b1d04aebd6d4c20
legendkong <jonathankong96@hotmail.com>
2022-01-09 23:23:34 +0800

    Lab 1 added! This is my first touch point with Java.

---
+++ Labs/src/BLKong_Lab_1.java
@@ -0,0 +1,20 @@
+//Objective : This is my first lab
+//Name : Kong Boon Lieng, Jonathan
+//Student number : 10237962
+//Tutorial group : T04F
+
+//File name : BLKong_Lab_1.java
+//Declaration : This is my own work and I did not copy or plagiarize people's work.
+
+public class BLKong_Lab_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to CSIT111!");
+		System.out.println("My name is Kong Boon Lieng, Jonathan");
+		System.out.println("This is my second semester in UOW");
+		System.out.println("My email is blkong001@mymail.sim.edu.sg");
+		System.out.println("I love to eat!");
+
+	}
+
+}
