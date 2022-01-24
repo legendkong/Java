a351606920a5c97f1e278110c19d2c7076f71768
legendkong <jonathankong96@hotmail.com>
2022-01-24 15:45:39 +0800

    My very first touch point with java. Hello, world!

--- Labs/src/HelloWorld.java
+++ Labs/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-
+	//My very first coding tutorial in school :)
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Welcome to CSIT111!");
